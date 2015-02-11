e14d8cae8b2d181a35daa2a2e7621204c2ba4530
Andrea Nardinocchi <andrea@nardinan.it>
2015-02-11 14:38:44 +0100

    A little bug has been fixed (typo)

--- extra.queries/calibrations.sql
+++ extra.queries/calibrations.sql
@@ -1,6 +1,10 @@
-SELECT t_device.kind, t_location.code, t_device.type, t_device.code, t_device_measurement.channel, t_device_measurement.pedestal, 
-	t_device_measurement.sigma_raw, t_device_measurement.sigma 
-FROM t_device_measurement
-	LEFT JOIN t_device_test ON (t_device_test.device_test_pk = t_device_measurement.device_test_fk)
-	LEFT JOIN t_device ON (t_device.device_pk = t_device_test.device_fk)
-	LEFT JOIN t_location ON (t_location.location_pk = t_device.location_fk);
+SELECT 
+	device.kind 			AS device_kind,
+	location.code 			AS device_location, 
+	device.type 			AS device_type, 
+	device.code 			AS device_code, 
+	device_test.date 		AS test_date, 
+	device_test.kind 		AS test_kind,
+FROM t_device_test			AS device_test
+	LEFT JOIN t_device 		AS device 	ON (device.device_pk = device_test.device_fk)
+	LEFT JOIN t_location 		AS location 	ON (location.location_pk = device.location_fk);
